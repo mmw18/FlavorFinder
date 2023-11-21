1259d18011bbe1774ae07e7c4d15d24dfffee2ca
ammann64 <austinmmann64@gmail.com>
2023-11-20 19:39:46 -0600

    Fix model require statement and css import error

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,28 +1,28 @@
-const term = document.querySelector('#flavor-search').value.trim();
+// const term = document.querySelector('#flavor-search').value.trim();
 
-const recipeSearchFormHandler = async (event) => {
-    event.preventDefault();
+// const recipeSearchFormHandler = async (event) => {
+//     event.preventDefault();
 
-    if (term) {
+//     if (term) {
         
-    }
-};
+//     }
+// };
 
-const restaurantSearchFormHandler = async (event) => {
-    event.preventDefault();
+// const restaurantSearchFormHandler = async (event) => {
+//     event.preventDefault();
 
-    if (term) {
+//     if (term) {
 
-    }
-};
+//     }
+// };
 
-document
-    .querySelector('#restaurant-search')
-    .addEventListener('submit', restaurantSearchFormHandler);
+// document
+//     .querySelector('#restaurant-search')
+//     .addEventListener('submit', restaurantSearchFormHandler);
 
-document
-    .querySelector('#recipe-search')
-    .addEventListener('submit', recipeSearchFormHandler);
+// document
+//     .querySelector('#recipe-search')
+//     .addEventListener('submit', recipeSearchFormHandler);
 
 
-module.exports = term;
+// module.exports = term;
